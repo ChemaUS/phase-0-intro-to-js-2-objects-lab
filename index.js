d66a8167e8933bc8b6e5d36b1c631b791c926294
Cesar Hernandez <chema27281@gmail.com>
2022-09-20 21:21:47 -0400

    commit tree 67c85477c569ef64ed59c18d7a967b81a105a789

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+const employee = {
+    name: "Cesar",
+    address: "214 conch st"
+}
+function updateEmployeeWithKeyAndValue(employee,key,value){
+     return {...employee,
+        [key]:value
+    }}
+    function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value){
+    employee[key]= value
+        return employee
+    }
+    function deleteFromEmployeeByKey(employee,key,value){
+        delete employee.key;
+        return{
+            ...employee,
+            [key]: value,
+        }
+    }
+    function destructivelyDeleteFromEmployeeByKey(employee,key,value){
+        delete employee.key;
+        employee[key]= value;
+        return employee
+    }
